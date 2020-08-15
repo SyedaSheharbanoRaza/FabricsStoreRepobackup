f586084e04e59e40a414cc582711884bafa2d386
SyedaSheharbanoRaza <57301432+SyedaSheharbanoRaza@users.noreply.github.com>
2020-08-15 23:51:49 +0500

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace FabricStore
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Shop_Name = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.Dues_TB = new System.Windows.Forms.TextBox();
+            this.Dues = new System.Windows.Forms.Label();
+            this.Price_TB = new System.Windows.Forms.TextBox();
+            this.Price = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Meter = new System.Windows.Forms.Label();
+            this.Brand_Code_TB = new System.Windows.Forms.TextBox();
+            this.Brand_Code = new System.Windows.Forms.Label();
+            this.Brand_name_TB = new System.Windows.Forms.TextBox();
+            this.Brand_Name = new System.Windows.Forms.Label();
+            this.Mobile_TB = new System.Windows.Forms.TextBox();
+            this.Mobile = new System.Windows.Forms.Label();
+            this.Cust_Name_TB = new System.Windows.Forms.TextBox();
+            this.Cust_Name = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::FabricStore.Properties.Resources.images__1_;
+            this.panel1.Location = new System.Drawing.Point(0, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1297, 77);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.Shop_Name);
+            this.panel2.Location = new System.Drawing.Point(869, 146);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(371, 100);
+            this.panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkRed;
+            this.label1.Location = new System.Drawing.Point(8, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(187, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Contact No# 0300-7295824";
+            // 
+            // Shop_Name
+            // 
+            this.Shop_Name.AutoSize = true;
+            this.Shop_Name.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Shop_Name.ForeColor = System.Drawing.Color.DarkRed;
+            this.Shop_Name.Location = new System.Drawing.Point(3, 10);
+            this.Shop_Name.Name = "Shop_Name";
+            this.Shop_Name.Size = new System.Drawing.Size(323, 31);
+            this.Shop_Name.TabIndex = 0;
+            this.Shop_Name.Text = "Muzammal Fabrics Dijkot";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.Dues_TB);
+            this.panel3.Controls.Add(this.Dues);
+            this.panel3.Controls.Add(this.Price_TB);
+            this.panel3.Controls.Add(this.Price);
+            this.panel3.Controls.Add(this.textBox1);
+            this.panel3.Controls.Add(this.Meter);
+            this.panel3.Controls.Add(this.Brand_Code_TB);
+            this.panel3.Controls.Add(this.Brand_Code);
+            this.panel3.Controls.Add(this.Brand_name_TB);
+            this.panel3.Controls.Add(this.Brand_Name);
+            this.panel3.Controls.Add(this.Mobile_TB);
+            this.panel3.Controls.Add(this.Mobile);
+            this.panel3.Controls.Add(this.Cust_Name_TB);
+            this.panel3.Controls.Add(this.Cust_Name);
+            this.panel3.Location = new System.Drawing.Point(12, 92);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(839, 237);
+            this.panel3.TabIndex = 2;
+            // 
+            // Dues_TB
+            // 
+            this.Dues_TB.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Dues_TB.Location = new System.Drawing.Point(174, 170);
+            this.Dues_TB.Name = "Dues_TB";
+            this.Dues_TB.Size = new System.Drawing.Size(208, 26);
+            this.Dues_TB.TabIndex = 13;
+            // 
+            // Dues
+            // 
+            this.Dues.AutoSize = true;
+            this.Dues.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Dues.ForeColor = System.Drawing.Color.DarkRed;
+            this.Dues.Location = new System.Drawing.Point(8, 170);
+            this.Dues.Name = "Dues";
+            this.Dues.Size = new System.Drawing.Size(53, 23);
+            this.Dues.TabIndex = 12;
+            this.Dues.Text = "Dues";
+            // 
+            // Price_TB
+            // 
+            this.Price_TB.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Price_TB.Location = new System.Drawing.Point(540, 130);
+            this.Price_TB.Name = "Price_TB";
+            this.Price_TB.Size = new System.Drawing.Size(151, 26);
+            this.Price_TB.TabIndex = 11;
+            // 
+            // Price
+            // 
+            this.Price.AutoSize = true;
+            this.Price.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Price.ForeColor = System.Drawing.Color.DarkRed;
+            this.Price.Location = new System.Drawing.Point(415, 128);
+            this.Price.Name = "Price";
+            this.Price.Size = new System.Drawing.Size(53, 23);
+            this.Price.TabIndex = 10;
+            this.Price.Text = "Price";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(174, 128);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(208, 26);
+            this.textBox1.TabIndex = 9;
+            // 
+            // Meter
+            // 
+            this.Meter.AutoSize = true;
+            this.Meter.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Meter.ForeColor = System.Drawing.Color.DarkRed;
+            this.Meter.Location = new System.Drawing.Point(5, 128);
+            this.Meter.Name = "Meter";
+            this.Meter.Size = new System.Drawing.Size(59, 23);
+            this.Meter.TabIndex = 8;
+            this.Meter.Text = "Meter";
+            // 
+            // Brand_Code_TB
+            // 
+            this.Brand_Code_TB.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Brand_Code_TB.Location = new System.Drawing.Point(540, 86);
+            this.Brand_Code_TB.Name = "Brand_Code_TB";
+            this.Brand_Code_TB.Size = new System.Drawing.Size(151, 26);
+            this.Brand_Code_TB.TabIndex = 7;
+            // 
+            // Brand_Code
+            // 
+            this.Brand_Code.AutoSize = true;
+            this.Brand_Code.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Brand_Code.ForeColor = System.Drawing.Color.DarkRed;
+            this.Brand_Code.Location = new System.Drawing.Point(415, 86);
+            this.Brand_Code.Name = "Brand_Code";
+            this.Brand_Code.Size = new System.Drawing.Size(109, 23);
+            this.Brand_Code.TabIndex = 6;
+            this.Brand_Code.Text = "Brand Code";
+            // 
+            // Brand_name_TB
+            // 
+            this.Brand_name_TB.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Brand_name_TB.Location = new System.Drawing.Point(174, 86);
+            this.Brand_name_TB.Name = "Brand_name_TB";
+            this.Brand_name_TB.Size = new System.Drawing.Size(208, 26);
+            this.Brand_name_TB.TabIndex = 5;
+            // 
+            // Brand_Name
+            // 
+            this.Brand_Name.AutoSize = true;
+            this.Brand_Name.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Brand_Name.ForeColor = System.Drawing.Color.DarkRed;
+            this.Brand_Name.Location = new System.Drawing.Point(4, 86);
+            this.Brand_Name.Name = "Brand_Name";
+            this.Brand_Name.Size = new System.Drawing.Size(114, 23);
+            this.Brand_Name.TabIndex = 4;
+            this.Brand_Name.Text = "Brand Name";
+            // 
+            // Mobile_TB
+            // 
+            this.Mobile_TB.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Mobile_TB.Location = new System.Drawing.Point(174, 44);
+            this.Mobile_TB.Name = "Mobile_TB";
+            this.Mobile_TB.Size = new System.Drawing.Size(208, 26);
+            this.Mobile_TB.TabIndex = 3;
+            // 
+            // Mobile
+            // 
+            this.Mobile.AutoSize = true;
+            this.Mobile.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Mobile.ForeColor = System.Drawing.Color.DarkRed;
+            this.Mobile.Location = new System.Drawing.Point(5, 44);
+            this.Mobile.Name = "Mobile";
+            this.Mobile.Size = new System.Drawing.Size(112, 23);
+            this.Mobile.TabIndex = 2;
+            this.Mobile.Text = "Mobile No#";
+            // 
+            // Cust_Name_TB
+            // 
+            this.Cust_Name_TB.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cust_Name_TB.Location = new System.Drawing.Point(174, 4);
+            this.Cust_Name_TB.Name = "Cust_Name_TB";
+            this.Cust_Name_TB.Size = new System.Drawing.Size(208, 26);
+            this.Cust_Name_TB.TabIndex = 1;
+            this.Cust_Name_TB.Text = "Customer Name";
+            // 
+            // Cust_Name
+            // 
+            this.Cust_Name.AutoSize = true;
+            this.Cust_Name.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cust_Name.ForeColor = System.Drawing.Color.DarkRed;
+            this.Cust_Name.Location = new System.Drawing.Point(4, 4);
+            this.Cust_Name.Name = "Cust_Name";
+            this.Cust_Name.Size = new System.Drawing.Size(151, 23);
+            this.Cust_Name.TabIndex = 0;
+            this.Cust_Name.Text = "Customer Name ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LemonChiffon;
+            this.ClientSize = new System.Drawing.Size(1252, 512);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Shop_Name;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox Brand_name_TB;
+        private System.Windows.Forms.Label Brand_Name;
+        private System.Windows.Forms.TextBox Mobile_TB;
+        private System.Windows.Forms.Label Mobile;
+        private System.Windows.Forms.TextBox Cust_Name_TB;
+        private System.Windows.Forms.Label Cust_Name;
+        private System.Windows.Forms.TextBox Dues_TB;
+        private System.Windows.Forms.Label Dues;
+        private System.Windows.Forms.TextBox Price_TB;
+        private System.Windows.Forms.Label Price;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label Meter;
+        private System.Windows.Forms.TextBox Brand_Code_TB;
+        private System.Windows.Forms.Label Brand_Code;
+    }
+}
+
